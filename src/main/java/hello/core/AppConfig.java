52b67c25126b3461be327bb254da806bd9a22b3f
kuk329 <74480236+kuk329@users.noreply.github.com>
2022-10-03 13:33:46 +0900

    스프링 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { // 실제 동작에 필요한 구현 객체를 생성한다.
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
-    private MemberRepository memberRepository() { // 사용자 정보 저장 db 결정
+    @Bean
+    public MemberRepository memberRepository() { // 사용자 정보 저장 db 결정
         return new MemoryMemberRepository(); // 여기서 구현체 넘겨줌.
     }
+
+    @Bean
     public DiscountPolicy discountPolicy(){ // 할인 정책 결정
         return new FixDiscountPolicy(); // 여기서 구현체 넘겨줌.
+   //     return new RateDiscountPolicy();
     }
 
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,15 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService=appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService=appConfig.memberService();
       //  MemberService memberService = new MemberServiceImpl();
-        Member member=new Member(1L,"memberA", Grade.VIP); // 멤버 객체 하나 생성.
 
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        Member member=new Member(1L,"memberA", Grade.VIP); // 멤버 객체 하나 생성.
         memberService.join(member); // 위에서 생성한 멤버 객체를 hashmap 객체에 저장.
 
         Member findMember =memberService.findMember(1L);
